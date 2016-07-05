cb1e2f99784c7dfb49025adc72656426428d8e9b
Michael de Groot <specamps@gmail.com>
2016-07-05 19:16:39 +0200

    deps added

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,27 @@ const packageJson  = require('./src/package.json')
 const gulpElectron = require('gulp-electron')
 const rimraf       = require('rimraf')
 const path         = require('path')
-const winInstaller = require('electron-windows-installer')
+const builder      = require("electron-builder")
+
+const electronVersion = '1.2.5'
+
+const Platform = builder.Platform
+
+process.NODE_ENV = 'test'
 
 gulp.task('default', cb => {
-  build(['win32-ia32', 'win32-x64', 'linux-ia32', 'linux-x64', 'darwin-x64'], cb)
+  rimraf('./build', () => {
+    build(['win32-ia32', 'win32-x64', 'linux-ia32', 'linux-x64', 'darwin-x64'], cb)
+  })
 })
 
-gulp.task('installer', cb => {
-  winInstaller({
-    appDirectory: './build/v0.37.6/win32-ia32/',
-    outputDirectory: './build/v0.37.6/installer/',
-    arch: 'ia32',
-  }).then(cb).catch(cb)
+gulp.task('installer', () => {
+  builder.build({
+    targets: Platform.WINDOWS.createTarget(),
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 })
 
 gulp.task('win32', cb => {
@@ -40,31 +49,31 @@ gulp.task('linux64', cb => {
 })
 
 const build = (platformsSet, cb) => {
-  rimraf('./build', () => {
-    var stream = gulp.src('').pipe(gulpElectron({
-      src:         './src',
-      packageJson: packageJson,
-      release:     './build',
-      cache:       './cache',
-      version:     'v0.37.6',
-      packaging:   true,
-      platforms:   platformsSet,
-      platformResources: {
-        darwin: {
-          CFBundleDisplayName: packageJson.name,
-          CFBundleIdentifier:  packageJson.name,
-          CFBundleName:        packageJson.name,
-          CFBundleVersion:     packageJson.version,
-        },
-        win: {
-          'version-string':  packageJson.version,
-          'file-version':    packageJson.version,
-          'product-version': packageJson.version,
-        },
+  var stream = gulp.src('').pipe(gulpElectron({
+    src:         './src',
+    packageJson: packageJson,
+    release:     './build',
+    cache:       './cache',
+    version:     'v' + electronVersion,
+    packaging:   true,
+    platforms:   platformsSet,
+    platformResources: {
+      darwin: {
+        CFBundleDisplayName: packageJson.name,
+        CFBundleIdentifier:  packageJson.name,
+        CFBundleName:        packageJson.name,
+        CFBundleVersion:     packageJson.version,
+        icon:                path.join(__dirname, 'src', 'inc', 'img', 'logo.png'),
       },
-    })).pipe(gulp.dest(''))
-    stream.on('end', function() {
-      cb()
-    })
+      win: {
+        'version-string':  packageJson.version,
+        'file-version':    packageJson.version,
+        'product-version': packageJson.version,
+        icon:              path.join(__dirname, 'src', 'inc', 'img', 'logo.png'),
+      },
+    },
+  })).pipe(gulp.dest(''))
+  stream.on('end', function() {
+    cb()
   })
 }
